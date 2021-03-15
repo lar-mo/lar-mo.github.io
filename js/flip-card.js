f513bddf1decd55c35e2b322baddc813364f8078
lar-mo <lmoiola@gmail.com>
2021-03-15 14:59:35 -0700

    merging files in to SPW

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $(".hero_image").click(function(){
+  $(".tiles").click(function(){
     $(this).toggle();
     $(this.nextElementSibling).fadeToggle(750);
   });
@@ -10,12 +10,16 @@ $(document).ready(function(){
     $(this.previousElementSibling).fadeToggle(750);
   });
 
-  $('header').click(function(){
-    $('body').load(location.href);
-  });
+  // $('header').click(function(){
+  //   $('body').load(location.href);
+  // });
 
-  $('.empty_row').click(function(){
-    $('body').load(location.href);
-  })
+  // $('.empty_row').click(function(){
+  //   $('body').load(location.href);
+  // })
+  //
+  // $('.close_link').click(function(){
+  //   $('.tiles').load(location.href);
+  // })
 
 });
