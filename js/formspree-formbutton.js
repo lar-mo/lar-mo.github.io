a973e586375b72e6b916e8861cd6f77cbe4f9784
lar-mo <lmoiola@gmail.com>
2021-03-16 11:40:23 -0700

    external js files

---
+++ js/formspree-formbutton.js
@@ -0,0 +1,45 @@
+window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+formbutton("create", {
+  theme: "classic",
+  title: "Contact me",
+  description: "",
+  styles: {
+    button: {
+      background: "#515151"
+    },
+    title: {
+      background: "#515151",
+      letterSpacing: "0.05em",
+      fontFamily:'"Raleway", cursive',
+    }
+  },
+    fields: [
+  {
+    name: "_subject",
+    type: "hidden",
+    label: "",
+    value: "message from lar-mo.io contact form",
+  },
+  {
+    name: "name",
+    type: "text",
+    label: "Name",
+    required: true,
+  },
+  {
+    name: "email",
+    type: "email",
+    label: "Email",
+    required: true,
+  },
+  {
+    name: "Message",
+    type: "textarea",
+  },
+  {
+    name: "Submit",
+    type: "submit",
+    value: "Submit Form",
+  }],
+  action: "https://formspree.io/xgepgqvg",
+})
---
+++ js/header-resizer.js
@@ -0,0 +1,15 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector("header").style.padding = "8px";
+    $('header').css('box-shadow',        '3px 3px 5px 4px rgba(25,25,25,0.4)')
+    $('header').css('-moz-box-shadow',   '3px 3px 5px 4px rgba(25,25,25,0.4)')
+    $('header').css('-webkit-box-shadow','3px 3px 5px 4px rgba(25,25,25,0.4)')
+  } else {
+    document.querySelector("header").style.padding = "19px";
+    $('header').css('box-shadow','')
+    $('header').css('-moz-box-shadow','')
+    $('header').css('-webkit-box-shadow','')
+  }
+}
